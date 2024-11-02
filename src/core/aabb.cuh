74bbaedbb928c0d55f44c7205731bcd9992e185e
Qianyue He <984041003@qq.com>
2024-11-03 00:01:21 +0800

    Reverted to faster non-shared mem version.

--- src/core/aabb.cuh
+++ src/core/aabb.cuh
@@ -44,11 +44,10 @@ public:
     CPT_CPU_GPU Vec3 centroid() const noexcept {return (maxi + mini) * 0.5f;}
     CPT_CPU_GPU Vec3 range()    const noexcept {return maxi - mini;}
 
-    CPT_GPU_INLINE bool intersect(const Ray& ray, float& t_near) const {
+    CPT_GPU bool intersect(const Ray& ray, float& t_near) const {
         auto t2s = ray.d.rcp(), o_div = ray.o * t2s;
-        auto t1s = 
-         (mini - ray.o) * t2s;             // long scoreboard
-        t2s = (maxi - ray.o) * t2s;
+        auto t1s = mini.fmsub(t2s, o_div);
+        t2s      = maxi.fmsub(t2s, o_div);
 
         float tmax = 0;
         t1s.min_max(t2s, t_near, tmax);
